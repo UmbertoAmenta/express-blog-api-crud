b225f63ce01bab0941a69842d21f921482daad21
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-07 00:23:31 +0100

    fix: static middleware for public folder

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
 
-const postsData = require("./data/posts.js")
-const postsRouter = require("./routers/posts.js")
-const routeNotFound = require("./middlewares/routeNotFound.js")
-const errorHandler = require("./middlewares/errorHandler.js")
+const postsData = require("./data/posts.js");
+const postsRouter = require("./routers/posts.js");
+const routeNotFound = require("./middlewares/routeNotFound.js");
+const errorHandler = require("./middlewares/errorHandler.js");
 
-app.use(express.static("pubblic"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-app.use("/posts", postsRouter)
+app.use("/posts", postsRouter);
 
 // Server
 app.get("/", (req, res) => {
-  res.send("Server del blog")
-})
+  res.send("Server del blog");
+});
 
-app.use(errorHandler)
-app.use(routeNotFound)
+app.use(errorHandler);
+app.use(routeNotFound);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})    
+  console.log(`Example app listening on port ${port}`);
+});
